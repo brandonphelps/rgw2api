ea19d68ba475b3460329b9d3279f3424458f2b1c
brandon phelps <bwp44f@mst.edu>
2021-01-07 20:12:26 -0600

    Updated the requires auth with more of the end points

--- src/uri.rs
+++ src/uri.rs
@@ -1,13 +1,10 @@
-use std::ops;
-
+#![allow(dead_code)]
 
 // should be usize?
 
-// todo: move this to some types thing? 
+// todo: move this to some types thing?
 struct ItemId(u128);
 struct RecipeId(u128);
-
-
 
 #[allow(non_camel_case_types)]
 enum EndPoint {
@@ -15,34 +12,35 @@ enum EndPoint {
     account_bank,
     items(ItemId),
     item_stats(ItemId),
-    item_stats_all, // this is nothing to do with a specific item. 
+    item_stats_all, // this is nothing to do with a specific item.
     recipes(RecipeId),
     build,
 }
 
 impl EndPoint {
     pub fn requires_auth(self) -> bool {
-	match self {
-	    EndPoint::account_materials => true,
-	    EndPoint::account_bank => true,
-	    EndPoint::items(_) => false,
-	}
+        match self {
+            // do require auth.
+            EndPoint::account_materials => true,
+            EndPoint::account_bank => true,
+
+            // don't require auth
+            EndPoint::items(_) => false,
+            EndPoint::item_stats(_) => false,
+            EndPoint::item_stats_all => false,
+            EndPoint::recipes(_) => false,
+            EndPoint::build => false,
+        }
     }
     pub fn uri(self) -> String {
-	match self {
-	    EndPoint::account_materials => "account/materials".to_string(),
-	    EndPoint::account_bank => "account/bank".to_string(),
-	    EndPoint::items(id) => format!("items/{}", id.0.to_string()),
-	    _ => unreachable!(),
-	}
+        match self {
+            EndPoint::account_materials => "account/materials".to_string(),
+            EndPoint::account_bank => "account/bank".to_string(),
+            EndPoint::items(id) => format!("items/{}", id.0.to_string()),
+            _ => unreachable!(),
+        }
     }
 }
-
-
-fn account_materials() {
-    let uri = "account/materials";
-}
-
 
 #[cfg(test)]
 mod test {
@@ -51,9 +49,9 @@ mod test {
 
     #[test]
     fn test_item_construction() {
-	let p = EndPoint::items(ItemId(3));
-	assert_eq!(p.uri(), "items/3");
-	let k = EndPoint::items(ItemId(1000));
-	assert_eq!(p.uri(), "items/1000");
+        let p = EndPoint::items(ItemId(3));
+        assert_eq!(p.uri(), "items/3");
+        let k = EndPoint::items(ItemId(1000));
+        assert_eq!(p.uri(), "items/1000");
     }
 }
